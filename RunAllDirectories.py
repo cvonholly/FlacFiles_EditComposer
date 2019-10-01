271572f1bbd7d247aed893d9810630c99fd26f78
tomdegai <44812683+tomdegai@users.noreply.github.com>
2019-10-01 17:04:42 +0200

    Update RunAllDirectories.py

--- RunAllDirectories.py
+++ RunAllDirectories.py
@@ -4,25 +4,23 @@ For Example: Johann Sebastain Bach --> Bach, Johann Sebastain
 
 Steps:
 
-1. You will have to install the mutagen module: https://mutagen.readthedocs.io/en/latest/
+1. You will have to install the mutagen module: https://mutagen.readthedocs.io/en/latest/user/index.html#
 
-2. Download all 3 python files in the same directory.
+2. Edit the directory the code should be apllied to (in "C:yourDirectory/).
 
-3. Edit the directory the code should be apllied to (in "C:yourDirectory/).
-
-4. In the 'editComposerFunction.py', enter the function you want to run.
+3. In the 'editComposerFunction.py', enter the function you want to run.
 
 5. Run this file.  
 """
 
 import os
-from editComposerFunction import editComposer
+from editComposerFunction import editTag
 
 
-for root, dirs, files in os.walk(r"C:yourDirectory/"):      
+for root, dirs, files in os.walk(r"Z:\24Bit\1. Classical"):      
     for filename in files:
         if filename.endswith(".flac"):
             try:
-                editComposer(os.path.join(root, filename)) 
+                editTag(os.path.join(root, filename)) 
             except KeyError:
-                print ("Composer Value is empty.")
+                print ("Tag Value is empty.")
